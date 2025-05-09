4bddef1c42415872e5aa93ba4398c774e400f04b
Miiross <metallicaemelhor@gmail.com>
2025-05-09 18:18:03 -0300

    PAM - jogo de Dados 0.1

---
+++ Dado/Dado.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dado
+{
+    class Dado
+    {
+        private Random random;
+
+        public Dado()
+        {
+            random = new Random();
+        }
+
+        public int Jogar()
+        {
+            return random.Next(1, 7);
+        }
+
+        public string GetImagem(int numero)
+        {
+            return $"dado{numero}.png";
+        }
+    }
+
+}
+
--- Dado/MainPage.xaml.cs
+++ Dado/MainPage.xaml.cs
@@ -2,28 +2,107 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+        private Dado meuDado = new Dado();
 
         public MainPage()
         {
             InitializeComponent();
         }
-
-        private void RollButton_Clicked(object sender, EventArgs e)
+        private async void RollButton_Clicked(object sender, EventArgs e)
         {
-            Random random = new Random();
-            int resultado = random.Next(1, 7);
-
-            dadoImagem.Source = $"dado{resultado}.png";
-
-
-            if (int.Parse(SelecaoPicker.SelectedItem.ToString()) == resultado)
+            if (SelecaoPicker.SelectedIndex == -1)
             {
-                DisplayAlert("Parabéns", "Você acertou o número!", "Ok");
+                await DisplayAlert("Aviso", "Escolha um número antes de rolar!", "Ok");
+                return;
+            }
+
+            await AnimarDado();
+
+            int resultado = meuDado.Jogar();
+            dadoImagem.Source = meuDado.GetImagem(resultado);
+
+            int escolha = int.Parse(SelecaoPicker.SelectedItem.ToString());
+
+            if (escolha == resultado)
+            {
+                await DisplayAlert("Parabéns", "Você acertou!", "Ok");
+
             }
             else
             {
-                DisplayAlert("Tente novamente", "Você errou! O número era " + resultado, "Ok");
+                await DisplayAlert("Tente novamente", $"Você escolheu {escolha}, mas saiu {resultado}.", "Ok");
+            }
+        }
+
+        private async void RollButtonY_Clicked(object sender, EventArgs e)
+        {
+            if (SelecaoPicker.SelectedIndex == -1)
+            {
+                await DisplayAlert("Aviso", "Escolha uma opção antes de jogar", "Ok");
+                return;
+            }
+
+            await AnimarDado();
+
+            int resultado = meuDado.Jogar();
+            dadoImagem.Source = meuDado.GetImagem(resultado);
+
+            string ResultadoY;
+            if (resultado <= 3)
+            {
+                ResultadoY = "1>3";
+            }
+            else
+            {
+                ResultadoY = "4>6";
+            }
+
+            int escolha = int.Parse(SelecaoPicker.SelectedItem.ToString());
+
+            if (escolha == resultado)
+            {
+                await DisplayAlert("Parabéns", "Você acertou!", "Ok");
+
+            }
+            else
+            {
+                await DisplayAlert("Tente novamente", $"Você escolheu {escolha}, mas saiu {ResultadoY}.", "Ok");
+            }
+        }
+
+        private async void RollButtonX_Clicked(object sender, EventArgs e)
+        {
+            if (SelecaoPicker.SelectedIndex == -1)
+            {
+                await DisplayAlert("Aviso", "Escolha um número antes de rolar!", "Ok");
+                return;
+            }
+
+            await AnimarDado();
+
+            int resultado = meuDado.Jogar();
+            dadoImagem.Source = meuDado.GetImagem(resultado);
+
+            int escolha = int.Parse(SelecaoPicker.SelectedItem.ToString());
+
+            if (escolha >= 4)
+            {
+                await DisplayAlert("Parabéns", "Você acertou!", "Ok");
+
+            }
+            else
+            {
+                await DisplayAlert("Tente novamente", $"Você escolheu {escolha}, mas saiu {resultado}.", "Ok");
+            }
+        }
+
+        private async Task AnimarDado()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                int numeroAleatorio = new Random().Next(1, 7);
+                dadoImagem.Source = $"dado{numeroAleatorio}.png";
+                await Task.Delay(80);
             }
         }
     }
