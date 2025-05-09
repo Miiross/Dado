95eb3f0e0b1f199935148eac6ee32c8dec272b8c
Miiross <metallicaemelhor@gmail.com>
2025-05-09 19:49:01 -0300

    PAM - Projeto dado

--- Dado/MainPage.xaml.cs
+++ Dado/MainPage.xaml.cs
@@ -35,64 +35,36 @@
         }
 
         private async void RollButtonY_Clicked(object sender, EventArgs e)
-        {
-            if (SelecaoPicker.SelectedIndex == -1)
-            {
-                await DisplayAlert("Aviso", "Escolha uma opção antes de jogar", "Ok");
-                return;
-            }
-
+        { 
             await AnimarDado();
 
             int resultado = meuDado.Jogar();
             dadoImagem.Source = meuDado.GetImagem(resultado);
 
-            string ResultadoY;
             if (resultado <= 3)
             {
-                ResultadoY = "1>3";
+                await DisplayAlert("Boa", "voce acertou!", "ok");
             }
             else
             {
-                ResultadoY = "4>6";
-            }
-
-            int escolha = int.Parse(SelecaoPicker.SelectedItem.ToString());
-
-            if (escolha == resultado)
-            {
-                await DisplayAlert("Parabéns", "Você acertou!", "Ok");
-
-            }
-            else
-            {
-                await DisplayAlert("Tente novamente", $"Você escolheu {escolha}, mas saiu {ResultadoY}.", "Ok");
+                await DisplayAlert("Ixi", "voce errou!", "ok");
             }
         }
 
         private async void RollButtonX_Clicked(object sender, EventArgs e)
         {
-            if (SelecaoPicker.SelectedIndex == -1)
-            {
-                await DisplayAlert("Aviso", "Escolha um número antes de rolar!", "Ok");
-                return;
-            }
-
             await AnimarDado();
 
             int resultado = meuDado.Jogar();
             dadoImagem.Source = meuDado.GetImagem(resultado);
 
-            int escolha = int.Parse(SelecaoPicker.SelectedItem.ToString());
-
-            if (escolha >= 4)
+            if (resultado >= 4)
             {
-                await DisplayAlert("Parabéns", "Você acertou!", "Ok");
-
+                await DisplayAlert("Boa", "voce acertou!", "ok");
             }
             else
             {
-                await DisplayAlert("Tente novamente", $"Você escolheu {escolha}, mas saiu {resultado}.", "Ok");
+                await DisplayAlert("Ixi", "voce errou!", "ok");
             }
         }
 
